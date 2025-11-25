e77280c495a0fed7329026ab733a6cb544f2ff3f
pap1000 <74577654+pap1000@users.noreply.github.com>
2025-11-25 09:11:53 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int A, B, RESULT;
+    cin >> A;
+    cin >> B;
+    
+    RESULT = A - B;
+    cout << RESULT << endl;
+    
+    return 0;
+}

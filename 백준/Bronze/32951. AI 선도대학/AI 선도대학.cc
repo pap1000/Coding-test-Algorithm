4c14d950447e409d3236c426bb33c4626105afec
Bong <74577654+pap1000@users.noreply.github.com>
2025-11-25 09:30:36 +0900

    [Bronze V] Title: AI 선도대학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int N;
+	cin >> N;
+	cout << N - 2024;
+
+	return 0;
+}

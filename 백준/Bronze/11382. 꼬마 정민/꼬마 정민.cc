19fb182b3d78873723cb78823bb07ed52e90e85c
Bong <74577654+pap1000@users.noreply.github.com>
2025-11-25 09:27:01 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    long long a, b, c;
+    long long result;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    result = a + b + c;
+    cout << result << endl;
+
+    return 0;
+}

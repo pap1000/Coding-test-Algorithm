2f410498dc8f6b5e127e8d434dc8034d3d0a240e
Bong <74577654+pap1000@users.noreply.github.com>
2025-11-25 09:27:34 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	int a, b, i;
+	cin >> i;
+	while (i > 0) {
+		cin >> a;
+		cin >> b;
+		cout << a + b << endl;
+		i--;
+	}
+
+	return 0;
+}

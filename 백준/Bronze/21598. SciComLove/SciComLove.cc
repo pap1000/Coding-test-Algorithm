6e4f09a1c42dd9517ac7622b532e22fa04686270
Bong <74577654+pap1000@users.noreply.github.com>
2025-11-25 09:29:29 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	int N;
+	string std = "SciComLove";
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cout << std << "\n";
+	}
+
+	return 0;
+}

06f3e26315c65aed3069ff83abc924f8a5ab3d21
Bong <74577654+pap1000@users.noreply.github.com>
2026-08-27 14:18:08 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -1,8 +1,3 @@
 def solution(array):
-    answer = []
-    max_num = max(array)
-    for i, num in enumerate(array):
-        if num == max_num:
-            answer.append(num)
-            answer.append(i)
-            return answer
+    idx, max_val = max(enumerate(array), key=lambda x: x[1])
+    return [max_val, idx]

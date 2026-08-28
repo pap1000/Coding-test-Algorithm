31a1c24bdeef3ec566b54b3533f411becb87419c
Bong <74577654+pap1000@users.noreply.github.com>
2026-08-28 12:37:44 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -0,0 +1,6 @@
+def solution(n):
+    piiiiiiizaaaaaaaaasssssssss = 0
+    while(1):
+        piiiiiiizaaaaaaaaasssssssss += n
+        if piiiiiiizaaaaaaaaasssssssss % 6 == 0:
+            return piiiiiiizaaaaaaaaasssssssss//6

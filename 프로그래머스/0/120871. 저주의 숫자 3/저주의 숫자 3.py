532bd23b3439033bffd8b6cfdf99f06b43d163f7
Bong <74577654+pap1000@users.noreply.github.com>
2026-08-31 13:37:33 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.02 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -0,0 +1,8 @@
+def solution(n):
+    answer = 0
+    for _ in range(n):
+        answer += 1
+        while '3' in str(answer) or answer%3 == 0:
+            answer += 1
+            
+    return answer

cfcbf77709de32c2779e1930f6c651a51eec0998
Bong <74577654+pap1000@users.noreply.github.com>
2026-09-05 13:38:12 +0900

    [level 3] Title: 단어 변환, Time: 0.38 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def solution(begin, target, words):
+    answer = 0
+    queue = deque([(begin, 0)])
+    visited = [False] * len(words)
+    
+    while queue:
+        curr, step = queue.popleft()
+        if curr == target:
+            return step
+        for i, w in enumerate(words):
+            if not visited[i]:
+                diff = sum(c1 != c2 for c1, c2 in zip(curr, w))
+                if diff == 1:
+                    visited[i] = True
+                    queue.append((w, step + 1))
+        
+    return 0

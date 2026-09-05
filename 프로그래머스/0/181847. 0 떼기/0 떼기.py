1e780445f050767f5817814472640704fe30def3
Bong <74577654+pap1000@users.noreply.github.com>
2026-09-05 12:38:20 +0900

    [level 0] Title: 0 떼기, Time: 0.00 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.py
@@ -0,0 +1,9 @@
+def solution(n_str):
+    while(1):
+        if n_str[0] == '0':
+            n_str = n_str[1:]
+            continue
+        else:
+            break
+            
+    return n_str

3681fa3ea3c24c3a5fb55e25e4a83abb57761c2d
Bong <74577654+pap1000@users.noreply.github.com>
2026-09-15 12:59:39 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/README.md
+++ 프로그래머스/1/12943. 콜라츠 추측/README.md
@@ -1,10 +1,10 @@
 # [level 1] 콜라츠 추측 - 12943 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943?language=cpp) 
 
 ### 성능 요약
 
-메모리: 11.2 MB, 시간: 0.06 ms
+메모리: 4.95 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 09월 15일 12:44:20
+2026년 09월 15일 12:59:36
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int num) {
+    int answer = 0;
+    long long n = num;
+    
+    while (n != 1 && answer < 500) {
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        answer += 1;
+    }
+    
+    if (answer == 500 && n != 1) {
+        return -1;
+    }
+    
+    return answer;
+}
